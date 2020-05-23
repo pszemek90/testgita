dd03aec2a0467d507a419e4bdfdeea73a3736c1e
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2020-05-23 16:31:33 +0200

    jakiś komentarz

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("coś dziwnego");
         System.out.println("po zmianie użytkownika");
         System.out.println("jeszcze jedna zmiana");
-        //jakis komentarz
+        //jakis komentar
+        //gjhgjhg
     }
 }
